354f505e9cfefef31882110086e158d213bd94e9
Pranavi <inkslinger5198@gmail.com>
2024-01-01 16:51:54 +0530

    first commit

---
+++ src/Confirm.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Confirm.css";
+
+const Confirm = () => {
+  return (
+    <div>
+      <div className="Confirm">
+      </div>
+    </div>
+  );
+};
+
+export default Confirm;
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import App from "./App";
+import Confirm from "./Confirm";
+import "./index.css";
+
+ReactDOM.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/confirm" element={<Confirm />} />
+    </Routes>
+  </Router>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
