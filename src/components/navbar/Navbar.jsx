a8fb03665739e8faf791d4c48e842eaace91fdd2
Pranavi <inkslinger5198@gmail.com>
2024-01-02 00:43:26 +0530

    hehehe navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
-import './navbar.css'
+import React from "react";
+import "./navbar.css";
+import Logo from "../../assets/3.png";
+import { Link, animateScroll as scroll } from "react-scroll";
+const Menu = () => (
+  <>
+    <p>
+      <Link to="nav" smooth={true} duration={100}>
+        Home
+      </Link>
+    </p>
+    <p>
+      <Link to="services" smooth={true} duration={100}>
+        Services
+      </Link>
+    </p>
+    <p>
+      <Link to="testimonial" smooth={true} duration={100}>
+        Wins
+      </Link>
+    </p>
+    <p>
+      <Link to="contact" smooth={true} duration={100}>
+        Contact
+      </Link>
+    </p>
+    <p>
+      <Link to="about" smooth={true} duration={100}>
+        About
+      </Link>
+    </p>
+  </>
+);
+
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+    <div className="navbar" id="nav">
+      <div className="navbar-logo">
+        <img src={Logo} alt="logo" />
+      </div>
 
-export default Navbar
+      <div className="navbar-links">
+        <Menu />
+      </div>
+
+      <div className="navbar-button">
+        <a
+          href="https://app.cal.com/pravitbh/progressor-media"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="btn-1">Get Started</button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
